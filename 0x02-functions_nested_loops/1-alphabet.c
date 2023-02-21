76f36280d975dc38123586d0ab5a5629361e4a23
francojean <francojean2812@gmail.com>
2023-02-21 15:20:27 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#inlude "main.h"
+
+/**
+ *  print_alphabet - print the alpha in lowercase
+ */
+voif print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_purchar(la);
+
+	_putchar('\n');
+}
